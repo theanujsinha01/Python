c8772656a86be0fef455a79f80ab46e03f42787d
Anuj kumar <theanujsinha@gmail.com>
2024-11-10 13:36:11 +0530

    commit

--- Strings/Strings.py
+++ Strings/Strings.py
@@ -1,3 +1,13 @@
-print('Anuj')
-print("Anuj")
+job = "software Engineer"
+print(job)
 
+#charactor in string at index
+print(job[0]) # first char
+print(job[1])
+print(job[2])
+print(job[-1]) #last char
+
+#multiline strings
+str = """We are learning Python
+Python is most powerful program"""
+print(str)
