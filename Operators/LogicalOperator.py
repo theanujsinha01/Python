19a7877f72512362ac821cf56557ab8cf36cd75e
Anuj kumar <theanujsinha@gmail.com>
2024-11-10 13:23:05 +0530

    commit

---
+++ Operators/LogicalOperator.py
@@ -0,0 +1,14 @@
+#Logical operators : Used to combine two or more conditional statement
+
+# and operator
+x = 10
+print(x<50 and x>1)
+
+# or operator
+y = 23
+print(y>20 or y<0)
+
+# not operator
+z = 10
+print(not z>100) # give always oposite
+
