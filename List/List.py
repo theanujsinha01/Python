abb98934aba3d2483b6d0f463e79aa76484dba4c
Anuj kumar <theanujsinha@gmail.com>
2024-11-10 14:11:37 +0530

    commit

---
+++ List/List.py
@@ -0,0 +1,31 @@
+# List : collection items with different or similar 
+list = [1, 'Anuj',2.5, True]
+print(list)
+
+print(list[0]) # first element of list
+print(list[-1]) # last element of list
+
+# Add element in list from last
+list.append('rohit')
+print(list)
+
+# Remove element in list
+list.remove('Anuj')
+print(list)
+
+# length of list
+print(len(list))
+
+# remove element from last
+list.pop()
+print(list)
+
+# insert element at specific index 
+list.insert(0,'ZDM')
+print(list)
+
+# clear the list
+list.clear()
+print(list)
+
+
---
+++ Tuple/tuple.py
@@ -0,0 +1,16 @@
+#Tuple : similar to list but can't modified once created
+a = (1,5,8)
+print(a)
+
+# count() : count the frequency of specific item
+tuple_1 = (1,2,4,2,2,9)
+print(tuple_1.count(2))
+
+#index() : search a specific value and give index
+ans = tuple_1.index(9)
+print(ans)
+
+# length of tuple
+print(len(tuple_1))
+
+
