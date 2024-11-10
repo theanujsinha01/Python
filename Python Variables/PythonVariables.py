e475b062381165268c565a65841c9141301fe7d1
Anuj kumar <theanujsinha@gmail.com>
2024-11-10 12:19:13 +0530

    commit

---
+++ Python Variables/PythonVariables.py
@@ -0,0 +1,11 @@
+# variables :
+# Name of memory location where contain valu or information
+
+greeting = 'good morning'
+print(greeting)
+
+
+number1 = 5
+number2 = 7.5
+total = number1+number2
+print(total)
