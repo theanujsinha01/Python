5d6e3902de3841cd706dad0051d0b8787824561f
Anuj kumar <theanujsinha@gmail.com>
2024-11-11 15:25:23 +0530

    commit

---
+++ Control and Flow/LoopingStatement.py
@@ -0,0 +1,30 @@
+ #for loop 
+
+mylist = ['anuj', 'rohan','sonu','monu']
+for it in mylist:
+    print(it)
+
+word = 'Anuj Kumar'
+for i in word:
+    print(i)
+
+
+
+# range() : generate sequence of numbers
+# range(5) : 0, 1, 2, 3, 4
+
+
+for i in range(5):
+    print(i)
+
+#print dict 
+person = {1 : 'rohit', 2:'mohit', 3:'sonu'}
+for i in person:
+    print(person[i])
+
+# while Loop 
+i = 1
+n =5
+while i<=n:
+    print(i)
+    i = i+1
---
+++ Control and Flow/condtionalStatements.py
@@ -0,0 +1,25 @@
+#if condition
+x = 7
+if x>5:
+    print("this is true")
+
+
+
+# if and else condition
+b = 38
+if b>8:
+    print('b is bigger then 8')
+else:
+    print('b is smaller then 8')
+
+# if , elif and else 
+y = 20
+if y>20:
+    print('y is bigger than 20')
+elif y<20:
+    print('y is smaller than 20')
+else:
+    print('y is equal to 20')
+
+
+
