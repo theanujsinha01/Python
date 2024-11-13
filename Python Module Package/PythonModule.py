903cb7d516cb684340ba190ca33620965fc3b4f8
Anuj kumar <theanujsinha@gmail.com>
2024-11-13 16:17:16 +0530

    commit

--- Python Module Package/PythonModule.py
+++ Python Module Package/PythonModule.py
@@ -1,5 +1,7 @@
 #module : single file with .py and this contains all the code like functions, classes and variables
 
+#package : collections of modules , contain sub-package
+
 # built-in module
 
 import math
@@ -9,4 +11,14 @@ print(sqrt)
 pi = math.pi
 print(pi)
 
+import datetime
+now = datetime.datetime.now()
+print(now)
 
+
+
+
+#create own module and package
+from myPackage import calc
+ans = calc.mul(3,5)
+print(ans)
