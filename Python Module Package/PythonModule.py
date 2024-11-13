2d7198110c3ce8505350d7a34a4657fbe1fcc3a3
Anuj kumar <theanujsinha@gmail.com>
2024-11-13 15:42:16 +0530

    commit

---
+++ Python Module Package/PythonModule.py
@@ -0,0 +1,12 @@
+#module : single file with .py and this contains all the code like functions, classes and variables
+
+# built-in module
+
+import math
+sqrt = math.sqrt(16)
+print(sqrt)
+
+pi = math.pi
+print(pi)
+
+
