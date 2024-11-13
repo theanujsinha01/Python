b42d0333094833048006afc322eb6c7ae18ead14
Anuj kumar <theanujsinha@gmail.com>
2024-11-13 15:30:07 +0530

    commit

--- Error Handling/ErrorHandling.py
+++ Error Handling/ErrorHandling.py
@@ -1,7 +1,23 @@
 #try-except, else, finally
 
-
+#try-except
 try:
     print(1/0)
 except Exception as e:
     print("You can not divide by zero", e)
+
+
+
+#else block and finally block 
+try:
+    a = 2/0
+    b = 2*1
+except ZeroDivisionError:
+    print("you can not divide by zero")
+except NameError:
+    print("Name occured")
+else:
+    print(a)
+    print(b)
+finally:
+    print("try-except ")
