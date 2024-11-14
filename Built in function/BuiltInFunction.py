06b09b3f9669f363765dad9afff271e57a6bbeb3
Anuj kumar <theanujsinha@gmail.com>
2024-11-14 15:32:59 +0530

    commit

---
+++ Built in function/BuiltInFunction.py
@@ -0,0 +1,30 @@
+# print()
+print("hello")
+
+print("Anuj \n kumar")
+
+# end....
+print("Anuj",end='##')
+print("    ")
+
+#sep...
+print("Rohit","sharma" ,sep="$$")
+
+# f string
+a = 100
+print(f"my marks is Rs {a}")
+
+#round()
+#round(number,ndigit)
+result = 4.6
+print(round(result))
+
+num = 6.7789899
+print(round(num,3))
+
+
+# range()
+# range(start, end, step)
+for i in range(1,10):
+    print(i)
+
