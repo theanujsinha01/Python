e5981cd09f35a45312ce475f3646d035a5ba1d3c
Anuj kumar <theanujsinha@gmail.com>
2024-11-20 15:47:16 +0530

    commit

---
+++ Eval Function/EvalFunction.py
@@ -0,0 +1,9 @@
+# You can use eval() to evaluate mathematical expressions provided as a string.
+# Define a mathematical expression as a string
+expression = "3 * (4 + 5)"
+
+# Evaluate the expression
+result = eval(expression)
+
+# Output the result
+print(result)  # Output: 27
---
+++ Sorted Function/sortedFunction.py
@@ -0,0 +1,3 @@
+numbers = [4, 2, 9, 1, 5]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)  # Output: [1, 2, 4, 5, 9]
