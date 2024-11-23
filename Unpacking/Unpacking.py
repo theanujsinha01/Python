cc59d09be2c40256021ee890ae40a63f2fbeef81
Anuj kumar <theanujsinha@gmail.com>
2024-11-23 14:34:09 +0530

    commit

--- Unpacking/Unpacking.py
+++ Unpacking/Unpacking.py
@@ -9,3 +9,29 @@ info = {'name': 'rohit', 'age': 21}
 for key, value in info.items():  # Unpacking dictionary items into key and value
     print(key)
     print(value)
+
+print("...........................................")
+sequence = ['rohit', 1, 2, 3, 4]
+
+# Unpacking the sequence. 'name' gets the first item, and 'marks' gets the rest.
+name, *marks = sequence
+print(name)  # Output: rohit
+
+# 'marks' holds the remaining elements of the list (the numbers 1, 2, 3, 4) as a list.
+print(marks)  # Output: [1, 2, 3, 4]
+
+
+print("...........................................")
+
+# items(), keys(), values()
+student ={'name':'Anuj','age':22,'grade':'A'}
+print(student.items())
+
+for key , value in student.items():
+    print(key, value)
+
+print("...........................................")
+name, age, grade = student.values()
+print(name)
+print(age)
+print(grade)
