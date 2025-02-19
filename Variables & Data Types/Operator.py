e4a92c6414c8d82bacaa3d49b474688b1c6234bb
Anuj kumar <theanujsinha@gmail.com>
2025-02-19 18:46:20 +0530

    commit

--- Variables & Data Types/Operator.py
+++ Variables & Data Types/Operator.py
@@ -1,4 +1,4 @@
-# Arithmetic Operators :
+# Arithmetic Operators (+,-,/,*,//,**,%) :
 a = 10
 b = 5
 print(a+b) # Output : 15
@@ -7,6 +7,7 @@ print(a*b) # Output : 50
 print(a/b) # Output : 2.0
 print(a**b) # Output : 100000 a^b
 print(a % b) # Output : 0
+print(5//2) # Output : 2 floor division operator
 
 #------------------------------------------------------------------------------------------
 
@@ -23,4 +24,28 @@ print(x <= y) # Output : False
 
 #------------------------------------------------------------------------------------------
 
+# Assignment Operator (=): 
 
+u = 100
+print("Value of U is :",u) #Value of U is : 100
+
+#------------------------------------------------------------------------------------------
+# Logical Operator (and, or , not):
+
+val1 = True
+val2 = False
+
+print(val1 and val2) #Output :False
+print(val1 or val2) #Output :True
+print(not val2) #Output :True
+
+#------------------------------------------------------------------------------------------
+# IN , NOT IN Operator :
+numbers = [1, 2, 3, 4, 5]
+print(3 in numbers)  # Output: True
+
+text = "Hello"
+print("H" in text)   # Output: True
+
+print(10 not in numbers)  # Output: True
+print("z" not in text)    # Output: True
