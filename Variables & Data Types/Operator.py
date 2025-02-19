1d8a4ca4feda6f2240b58a9708b85817d4ab1934
Anuj kumar <theanujsinha@gmail.com>
2025-02-19 18:33:17 +0530

    commit

---
+++ Variables & Data Types/Operator.py
@@ -0,0 +1,26 @@
+# Arithmetic Operators :
+a = 10
+b = 5
+print(a+b) # Output : 15
+print(a-b) # Output : 5
+print(a*b) # Output : 50
+print(a/b) # Output : 2.0
+print(a**b) # Output : 100000 a^b
+print(a % b) # Output : 0
+
+#------------------------------------------------------------------------------------------
+
+# Comparison Operators (==,>,<, >=,<=,!=):
+x = 10
+y = 8
+
+print(x == y) # Output : False
+print(x != y) # Output : True
+print(x > y) # Output : True
+print(x < y) # Output : False
+print(x >= y) # Output : True
+print(x <= y) # Output : False
+
+#------------------------------------------------------------------------------------------
+
+
