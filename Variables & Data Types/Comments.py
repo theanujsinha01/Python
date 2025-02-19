cd62273cd4db614ac7037c56d605a0d04dc4be6f
Anuj kumar <theanujsinha@gmail.com>
2025-02-19 18:13:51 +0530

    commit

---
+++ Variables & Data Types/Comments.py
@@ -0,0 +1,10 @@
+print("Single line comment")
+
+# Hello How are you ?
+
+print("Multi line Comments")
+
+"""How are you ?
+   what is your name ?
+   where are you now ?
+"""
---
+++ Variables & Data Types/DataTypes.py
@@ -0,0 +1,11 @@
+# Integer
+# Float
+# Boolean
+# String
+# None
+
+x = 10 # Integer
+pi = 3.14 # Float
+isHappy = True # Boolean
+name = "Ravi" # String
+z = None # None
