045580ccaaedb2af216930a0dbd42e8676964d45
Anuj kumar <theanujsinha@gmail.com>
2025-02-19 18:02:20 +0530

    commit

--- Variables & Data Types/Variable.py
+++ Variables & Data Types/Variable.py
@@ -3,4 +3,7 @@ age = 22
 print("My name is :",name) # Output : My name is : Anuj kumar
 print("My age is :",age) # Output : My age is : 22
 
+print(type(name)) # Output : <class 'str'>
+print(type(age)) # Output : <class 'int'>
+
 
