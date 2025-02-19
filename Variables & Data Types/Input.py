83a10dfebce92018d6e7a4d79c1fd7f8e1a47c8a
Anuj kumar <theanujsinha@gmail.com>
2025-02-19 19:04:33 +0530

    commit

---
+++ Variables & Data Types/Input.py
@@ -0,0 +1,11 @@
+# input() always take as string 
+
+a = input("Enter a Num1 : ")
+print(type(a)) #Output : <class 'str'>
+
+b = int(input("Enter a Num2 :"))
+print(type(b)) #Output : <class 'int'>
+
+c = float(input("Enter a Num3 :"))
+print(type(c)) #Output : <class 'float'>
+
