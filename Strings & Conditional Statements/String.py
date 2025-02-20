27438da4c827523c58dac82898b28aa3785651a4
Anuj kumar <theanujsinha@gmail.com>
2025-02-20 18:43:22 +0530

    commit

---
+++ Strings & Conditional Statements/String.py
@@ -0,0 +1,32 @@
+str1 = "This is a string.\n We are creating it in python"
+print(str1)
+
+
+#concatination 
+str2 = "Anuj"
+str3 = "Kumar"
+print(str2 + str3) #Output : AnujKumar
+
+# Length :
+len2 = len(str2)
+len3 = len(str3)
+print(len2,len3) #Output : 4 5
+
+# Indexing :
+strr = "HelloWorld"
+print(strr[0]) #Output : H
+print(strr[-1]) #Output : d
+
+
+# Slicing string_name[start : End : step] :
+a = "Python is a high level language"
+print(a[0:]) #Output : Python is a high level language
+
+print(a[9:17]) #Output : a high 
+print(a[: : -1]) #Output : egaugnal level hgih a si nohtyP
+
+
+
+
+
+
