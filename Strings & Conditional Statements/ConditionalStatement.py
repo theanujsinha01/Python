cd75df44bdfc443af90f59836b2b624082e8c7a9
Anuj kumar <theanujsinha@gmail.com>
2025-02-20 19:05:57 +0530

    commit

---
+++ Strings & Conditional Statements/ConditionalStatement.py
@@ -0,0 +1,25 @@
+
+light = "Yellow"
+
+if light == "Green":
+    print("Go") 
+elif light == "Yellow":
+    print("Look") #Output : Look
+else:
+    print("Stop")
+
+
+marks = 83
+if marks >= 90:
+    grade = "A"
+elif marks >= 80:
+    grade = "B"
+elif marks >= 70:
+    grade = "C"
+else :
+    grade = "D"
+
+print("grade of the student is",grade) #Output :grade of the student is B
+
+
+
