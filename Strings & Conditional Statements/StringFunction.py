d7d3e6723021c61a4d5861df194fff9db8e1c302
Anuj kumar <theanujsinha@gmail.com>
2025-02-20 18:53:48 +0530

    commit

---
+++ Strings & Conditional Statements/StringFunction.py
@@ -0,0 +1,17 @@
+
+str = "im studying python from collage walla"
+str = str.capitalize()
+print(str)
+
+# replace replace(old,new):
+str = str.replace("p","N")
+print(str) # Output : Im studying Nython from collage walla
+
+# Find :
+print(str.find("walla")) #Output : 32
+
+
+# Count :
+print(str.count("t")) #Output : 2
+
+
