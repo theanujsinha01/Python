0004876cdc22568e0a8fbcc10224756282bd0b8a
Anuj kumar <theanujsinha@gmail.com>
2025-03-14 19:27:18 +0530

    commit

---
+++ Functions/Function.py
@@ -0,0 +1,14 @@
+
+def calc_sum(a, b):
+    print("Sum of a and b is: ", a + b)
+    
+
+def calc_diff(a, b):
+    print("Difference of a and b is: ", a - b)
+
+
+calc_sum(10, 5) # Output: 15
+calc_diff(10, 5) # Output: 5
+
+
+
